850053202756e4af72d8814da70f707e05778472
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-01 16:02:35 +0000

    New translations main.py (Dutch)

--- nl-NL/code/powerful_patterns_random_faces_example/main.py
+++ nl-NL/code/powerful_patterns_random_faces_example/main.py
@@ -4,20 +4,20 @@ from p5 import *
 from random import randint
 
 
-def draw_motif():
-    orange = Color(191, 64, 191)
-    brown = Color(200, 120, 0)
-    green = Color(100, 155, 0)
-    fill(orange)
+def teken_motief():
+    oranje = Color(191, 64, 191)
+    bruin = Color(200, 120, 0)
+    groen = Color(100, 155, 0)
+    fill(oranje)
     ellipse(200, 200, 200, 190)
     fill(0)
-    # Eyes
+    # Ogen
     ellipse(160, 190, 30, 30)
     ellipse(240, 190, 30, 30)
     fill(255)
     ellipse(165, 200, 10, 10)
     ellipse(245, 200, 10, 10)
-    # Mouth
+    # Mond
     no_fill()
     stroke(255, 255, 255)
     ellipse(150, 250, 30, 30)
@@ -37,10 +37,10 @@ def setup():
 
 def draw():
     push_matrix()
-    # offset by the width of the quarter-size face
+    # gecompenseerd door de breedte van het kwartgrote gezicht
     translate(randint(-50, 350), randint(-50, 350))
-    scale(0.25, 0.25)  # quarter size paths
-    draw_motif()
+    scale(0.25, 0.25) # kwart grootte paden
+    teken_motief()
     pop_matrix()
 
 
