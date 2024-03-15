a1c4fe697f205a9b1f477f6583e9713236947301
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-15 14:26:18 +0000

    New translations main.py (Polish)

--- pl-PL/code/powerful_patterns_kek_lepis_sarawak/main.py
+++ pl-PL/code/powerful_patterns_kek_lepis_sarawak/main.py
@@ -3,62 +3,62 @@
 from p5 import *
 from time import *
 
-# Based on the amazing Malaysian geometric cake art: Kek lapis Sarawak
+# Na podstawie niesamowitej sztuki malezyjskiego geometrycznego ciasta: Kek lapis Sarawak
 
 
-def quadrant():
-    # Choose some gorgeous colours for the cake layers
-    turquoise = Color(64, 224, 208)
-    gold = Color(255, 215, 0)
-    tomato = Color(255, 99, 71)
+def kwadrant():
+    # Wybierz kilka wspaniałych kolorów dla warstw ciasta
+    Turkus = Kolor(64, 224, 208)
+    Złoto = Kolor(255, 215, 0)
+    Pomidor = Kolor(255, 99, 71)
 
-    # Jam sticks the layers together
-    jam = Color(255, 165, 0)
-    stroke(jam)
-    stroke_weight(2)  # Change the number to change the amount of jam
+    # Zacięcie skleja warstwy razem
+    Zacięcie = Kolor(255, 165, 0)
+    skok(zacięcie)
+    Stroke_weight(2) # Zmień liczbę, aby zmienić ilość zacięcia
 
-    # Nine layers of cake, repeating the 3 colours 3 times
+    # Dziewięć warstw ciasta, powtarzając 3 kolory 3 razy
     for i in range(3):
-        start_y = i * 60  # height of 3 blocks of cake
-        fill(turquoise)
+        start_y = i * 60 # wysokość 3 bloków ciasta
+        wypełnienie(turkusowy)
         rect(0, start_y, 180, 20)
-        fill(gold)
+        fill(złoto)
         rect(0, start_y + 20, 180, 20)
-        fill(tomato)
+        fill(pomidor)
         rect(0, start_y + 40, 180, 20)
 
 
 def outer():
-    # The cake is wrapped in an outer layer
-    yellowgreen = Color(154, 205, 50)
+    # Ciasto jest owinięte w zewnętrzną warstwę
+    Żółtozielony = Kolor(154, 205, 50)
 
-    no_fill()  # Don't cover up the cake quadrants!
-    stroke(yellowgreen)
+    No_fill() # nie zakrywaj kwadrantów ciasta!
+    stroke(żółto-zielony)
     stroke_weight(20)
     rect(10, 10, 380, 380, 20)
 
 
 def setup():
-    size(400, 400)  # make the cake square
-    background(255, 255, 255, 0)  # transparent background
+    rozmiar(400, 400) # zrób ciasto kwadratem
+    tło(255, 255, 255, 0) # przezroczyste tło
 
 
 def draw():
-    # Define a quarter turn so our code is easy to read
-    quarter = radians(90)
+    # Zdefiniuj ćwierć obrotu, aby nasz kod był łatwy do odczytania
+    ćwiartka = radiany(90)
 
-    translate(200, 200)  # start from the center
+    translate(200, 200) # start od środka
 
-    # Make the bottom right quarter of the cake then rotate for the other quarters
+    # Dopasuj prawą dolną ćwiartkę ciasta, a następnie obróć o pozostałe ćwiartki
 
-    if frame_count <= 4:  # draw up to 4 quadrants
+    if frame_count <= 4: # narysuj do 4 kwadrantów
         for i in range(frame_count):
-            quadrant()
-            rotate(quarter)
+            kwadrant()
+            obróć(ćwiartka)
 
-    if frame_count == 5:  # add the outer layer
-        translate(-200, -200)  # back to the top corner
-        outer()  # outer layer
+    if frame_count == 5: # dodaj zewnętrzną warstwę
+        przetłumacz(-200, -200) # z powrotem do górnego rogu
+        zewnętrzna() # warstwa
 
 
-run(frame_rate=5)  # 5 frames per second
+run(frame_rate=5) # 5 klatek na sekundę
