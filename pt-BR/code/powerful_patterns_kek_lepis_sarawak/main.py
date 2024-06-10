cb00959d416a9acc568d34c83b33a2a8be186fa9
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-06-10 18:54:29 +0100

    New translations main.py (Portuguese, Brazilian)

--- pt-BR/code/powerful_patterns_kek_lepis_sarawak/main.py
+++ pt-BR/code/powerful_patterns_kek_lepis_sarawak/main.py
@@ -3,62 +3,62 @@
 from p5 import *
 from time import *
 
-# Based on the amazing Malaysian geometric cake art: Kek lapis Sarawak
+# Baseado na incrível arte geométrica do bolo da Malásia: Kek lapis Sarawak
 
 
-def quadrant():
-    # Choose some gorgeous colours for the cake layers
-    turquoise = Color(64, 224, 208)
-    gold = Color(255, 215, 0)
-    tomato = Color(255, 99, 71)
+def quadrante():
+    # Escolha algumas cores lindas para as camadas do bolo
+    turquesa = Color(64, 224, 208)
+    ouro = Color(255, 215, 0)
+    tomate = Color(255, 99, 71)
 
-    # Jam sticks the layers together
-    jam = Color(255, 165, 0)
-    stroke(jam)
-    stroke_weight(2)  # Change the number to change the amount of jam
+    # Geleia gruda as camadas juntas
+    geleia = Color(255, 165, 0)
+    stroke(geleia)
+    stroke_weight(2)  # Altere o número para alterar a quantidade de geleia
 
-    # Nine layers of cake, repeating the 3 colours 3 times
+    #Nove camadas de bolo, repetindo as 3 cores 3 vezes
     for i in range(3):
-        start_y = i * 60  # height of 3 blocks of cake
-        fill(turquoise)
-        rect(0, start_y, 180, 20)
-        fill(gold)
-        rect(0, start_y + 20, 180, 20)
-        fill(tomato)
-        rect(0, start_y + 40, 180, 20)
+        iniciar_y = i * 60  # altura de 3 blocos de bolo
+        fill(turquesa)
+        rect(0, iniciar_y, 180, 20)
+        fill(ouro)
+        rect(0, iniciar_y + 20, 180, 20)
+        fill(tomate)
+        rect(0, iniciar_y + 40, 180, 20)
 
 
-def outer():
-    # The cake is wrapped in an outer layer
-    yellowgreen = Color(154, 205, 50)
+def exterior():
+    # O bolo é embrulhado em uma camada externa
+    amareloverde = Color(154, 205, 50)
 
-    no_fill()  # Don't cover up the cake quadrants!
-    stroke(yellowgreen)
+    no_fill()  # Não cubra os quadrantes do bolo!
+    stroke(amareloverde)
     stroke_weight(20)
     rect(10, 10, 380, 380, 20)
 
 
 def setup():
-    size(400, 400)  # make the cake square
-    background(255, 255, 255, 0)  # transparent background
+    size(400, 400)  # faça o bolo quadrado
+    background(255, 255, 255, 0)  # fundo transparente
 
 
 def draw():
-    # Define a quarter turn so our code is easy to read
-    quarter = radians(90)
+    # Defina um quarto de volta para que nosso código seja fácil de ler
+    quarto = radians(90)
 
-    translate(200, 200)  # start from the center
+    translate(200, 200)  # comece do centro
 
-    # Make the bottom right quarter of the cake then rotate for the other quarters
+    # Faça o quadrado inferior direito do bolo e gire para os outros quadrados
 
-    if frame_count <= 4:  # draw up to 4 quadrants
+    if frame_count <= 4:  # desenhar até 4 quadrantes
         for i in range(frame_count):
-            quadrant()
-            rotate(quarter)
+            quadrante()
+            rotate(quarto)
 
-    if frame_count == 5:  # add the outer layer
-        translate(-200, -200)  # back to the top corner
-        outer()  # outer layer
+    if frame_count == 5:  # adicione a camada externa
+        translate(-200, -200)  # de volta ao canto superior
+        exterior()  # camada externa
 
 
-run(frame_rate=5)  # 5 frames per second
+run(frame_rate=5)  # 5 quadros por segundo
