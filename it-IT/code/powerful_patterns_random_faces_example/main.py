7038dccc82a133e7e0df2ebfde37368aab5181f8
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-09-18 19:05:37 +0100

    New translations main.py (Italian)

--- it-IT/code/powerful_patterns_random_faces_example/main.py
+++ it-IT/code/powerful_patterns_random_faces_example/main.py
@@ -4,20 +4,20 @@ from p5 import *
 from random import randint
 
 
-def draw_motif():
-    orange = Color(191, 64, 191)
-    brown = Color(200, 120, 0)
-    green = Color(100, 155, 0)
-    fill(orange)
+def disegna_motivo():
+    arancio = Color(191, 64, 191)
+    marrone = Color(200, 120, 0)
+    verde= Color(100, 155, 0)
+    fill(arancio)
     ellipse(200, 200, 200, 190)
     fill(0)
-    # Eyes
+    # Occhi
     ellipse(160, 190, 30, 30)
     ellipse(240, 190, 30, 30)
     fill(255)
     ellipse(165, 200, 10, 10)
     ellipse(245, 200, 10, 10)
-    # Mouth
+    # Bocca
     no_fill()
     stroke(255, 255, 255)
     ellipse(150, 250, 30, 30)
@@ -37,10 +37,10 @@ def setup():
 
 def draw():
     push_matrix()
-    # offset by the width of the quarter-size face
+    # scostamento dal bordo pari ad un quarto della larghezza della faccia
     translate(randint(-50, 350), randint(-50, 350))
-    scale(0.25, 0.25)  # quarter size paths
-    draw_motif()
+    scale(0.25, 0.25) # scalatura ad un quarto della dimensione
+    disegna_motivo()
     pop_matrix()
 
 
