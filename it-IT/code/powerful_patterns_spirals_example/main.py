7e5ddf2c08ae4dd5980b8dc6f64182966ac4ef36
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-09-18 19:06:04 +0100

    New translations main.py (Italian)

--- it-IT/code/powerful_patterns_spirals_example/main.py
+++ it-IT/code/powerful_patterns_spirals_example/main.py
@@ -5,29 +5,29 @@ from math import random
 from random import randint
 
 
-def motif():
+def motivo():
     fill(randint(0, 255), randint(0, 255), randint(0, 255))
     ellipse(0, 0, 25, 25)
     fill(0, 0, 0)
     ellipse(0, 0, 15, 15)
     fill(randint(0, 255), randint(0, 255), randint(0, 255))
-    for i in range(4):  # a short row of squares
+    for i in range(4): # una breve fila di quadrati
         rect(i * 5, 0, 5, 5)
 
 
 def setup():
     size(400, 400)
-    stroke_weight(2)  # thick border
+    stroke_weight(2)  # bordo spesso
     background(255)
 
 
 def draw():
-    translate(200, 200)  # start from the centre of the screen
+    translate(200, 200) # inizia dal centro dello schermo
     if frame_count < 150:
-        for i in range(frame_count):  # animates the pattern
-            motif()
-            rotate(5)  # turns the motif
-            translate(i, i)  # moves the motif
+        for i in range(frame_count): # anima il motivo geometrico
+            motivo()
+            rotate(5) # gira il motivo base
+            translate(i, i) # sposta il motivo base
 
 
-run(frame_rate=10)  # fast animation
+run(frame_rate=10) # animazione veloce
