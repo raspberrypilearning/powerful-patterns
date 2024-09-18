d0ded0f7288c87521491571694d0abddb1256835
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-09-18 19:04:56 +0100

    New translations main.py (Arabic)

--- ar-SA/code/powerful_patterns_kek_lepis_sarawak/main.py
+++ ar-SA/code/powerful_patterns_kek_lepis_sarawak/main.py
@@ -7,17 +7,17 @@ from time import *
 
 
 def quadrant():
-    # Choose some gorgeous colours for the cake layers
+    # اختر بعض الألوان الرائعة لطبقات الكيك
     turquoise = Color(64, 224, 208)
     gold = Color(255, 215, 0)
     tomato = Color(255, 99, 71)
 
-    # Jam sticks the layers together
+    # المربى تلتصق الطبقات ببعضها البعض
     jam = Color(255, 165, 0)
     stroke(jam)
     stroke_weight(2)  # Change the number to change the amount of jam
 
-    # Nine layers of cake, repeating the 3 colours 3 times
+    # تسع طبقات من الكيك بتكرار 3 ألوان 3 مرات
     for i in range(3):
         start_y = i * 60  # height of 3 blocks of cake
         fill(turquoise)
@@ -29,7 +29,7 @@ def quadrant():
 
 
 def outer():
-    # The cake is wrapped in an outer layer
+    # الكعكة ملفوفة في طبقة خارجية
     yellowgreen = Color(154, 205, 50)
 
     no_fill()  # Don't cover up the cake quadrants!
@@ -44,12 +44,12 @@ def setup():
 
 
 def draw():
-    # Define a quarter turn so our code is easy to read
+    # حدد ربع دورة حتى يسهل قراءة التعليمات البرمجية الخاص بنا
     quarter = radians(90)
 
     translate(200, 200)  # start from the center
 
-    # Make the bottom right quarter of the cake then rotate for the other quarters
+    # اصنع الربع الأيمن السفلي من الكعكة ثم قم بتدوير الأرباع الأخرى
 
     if frame_count <= 4:  # draw up to 4 quadrants
         for i in range(frame_count):
