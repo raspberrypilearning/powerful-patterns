633ede465dd55269086c13cb1625cc67d83ffd73
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-09-18 19:09:50 +0100

    New translations main.py (Italian)

---
+++ it-IT/solutions/Spirals/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+from p5 import *
+from math import random
+from random import randint
+
+def motif():
+  fill(randint(0, 255),randint(0, 255) ,randint(0, 255))
+  ellipse(0, 0, 25, 25) 
+  fill(0, 0, 0)
+  ellipse(0, 0, 15, 15) 
+  fill(randint(0, 255),randint(0, 255) ,randint(0, 255))
+  for i in range(4): # una breve fila di quadrati
+    rect(i * 5, 0, 5, 5) 
+
+def setup():
+  size(400, 400) 
+  frame_rate(10) # animazione veloce
+  stroke_weight(2) # bordo spesso
+  background(255)
+  
+def draw():
+  translate(200, 200) # inizia dal centro dello schermo
+  if frame_count < 150:
+    for i in range(frame_count): # anima il motivo geometrico
+      motif()
+      rotate(5) # gira il motivo base
+      translate(i, i) # sposta il motivo base
+  
+run()
