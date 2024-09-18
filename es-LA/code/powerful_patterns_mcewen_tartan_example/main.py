656ec96d7edf8104686f401360ace04375b0c9c1
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-09-18 19:05:58 +0100

    New translations main.py (Spanish, Latin America)

--- es-LA/code/powerful_patterns_mcewen_tartan_example/main.py
+++ es-LA/code/powerful_patterns_mcewen_tartan_example/main.py
@@ -10,21 +10,21 @@ def setup():
 def draw():
     lines = 10 * frame_count  # Use in shape width/length to animate over time
 
-    # McEwen tartan colours
-    # Base square colours
+    # Colores del tartán McEwen
+    # Colores base de los cuadrados
     BLUE = Color(83, 143, 200)
     GREEN = Color(78, 163, 162)
     BASE_COLORS = [GREEN, BLUE]
 
-    # Cross colours
+    # Cruzar colores
     YELLOW = Color(155, 176, 135)
     RED = Color(155, 129, 113)
     CROSS_COLORS = [YELLOW, RED]
 
-    # Stitching and overlap colour
+    # Color de costura y superposición
     GREY = Color(78, 99, 86)
 
-    # Draw all the GREEN and BLUE alternating Base squares
+    # Dibujar todos los cuadrantes base que alternan entre VERDE y AZUL
     no_stroke()
     y_coordinate = 0
     squares = width/square_size
@@ -37,7 +37,7 @@ def draw():
             gap = gap + square_size
         y_coordinate = y_coordinate + square_size
 
-    # Crosses
+    # Cruces
     stroke(GREY)
 
     # DRAW THE YELLOW and RED alternating crosses
@@ -57,7 +57,7 @@ def draw():
             rect(0, cross, lines, 4)
             cross = cross + square_size
 
-    # Draw the grey lines where material overlaps
+    # Dibujar las líneas grises donde se superpone el material
     no_stroke()
     fill(GREY, 100)
     gap = square_size - 4
@@ -72,6 +72,6 @@ def draw():
 
 print('🏴󠁧󠁢󠁳󠁣󠁴󠁿󠁢󠁳󠁣󠁴󠁿 This is McEwen Tartan 🏴󠁧󠁢󠁳󠁣󠁴󠁿󠁧󠁢󠁳󠁣󠁴󠁿')
 square_size = int(
-    input('What size 🏴󠁧󠁢󠁳󠁣󠁴󠁿tartan would you like? 20, 50, or 100'))
+    input('What size 🏴󠁧󠁢󠁳󠁣󠁴󠁿tartan would you like? 20, 50 o 100'))
 
 run(frame_rate=10)
