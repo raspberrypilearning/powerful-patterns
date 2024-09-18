fd210f7ff1ce75fb7c22d950f4e6ac9d300b706e
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-09-18 19:06:33 +0100

    New translations main.py (Spanish, Latin America)

--- es-LA/code/powerful_patterns_starter/main.py
+++ es-LA/code/powerful_patterns_starter/main.py
@@ -5,16 +5,16 @@ from random import randint
 
 
 def setup():
-    # Put code to run once here
+    # Ejecuta el código aquí una vez
     size(400, 400)
     background(255, 255, 255)
 
 
 def draw():
-    # Put code to run every frame here
+    # Ejecuta el código para cada cuadro aquí
     fill(255, 0, 255)
     rect(50, 50, 120, 100)
 
 
-# Keep this to run your code
+# Mantén esto para ejecutar tu código
 run(frame_rate=5)
