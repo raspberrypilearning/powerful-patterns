5d3b92d9d19b25a87dd6da2cb6c8c56034c99256
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-05 20:26:41 +0000

    New translations main.py (French)

--- fr-FR/solutions/Art deco wallpaper/main.py
+++ fr-FR/solutions/Art deco wallpaper/main.py
@@ -7,7 +7,7 @@ from random import randint
 def motif():
   global taille_cercle
   for i in range(5):
-    ellipse(0, 0, circle_size / 5 * (5 - i), circle_size / 5 * (5  - i)) 
+    ellipse(0, 0, taille_cercle / 5 * (5 - i), taille_cercle / 5 * (5 - i)) 
 
 def setup():
   size(400, 400)
@@ -21,17 +21,17 @@ def setup():
 def draw():
   
   # Couleurs du motif
-  stroke(40, 35, 100) # blue
-  stroke_weight(2) # thick border
-  fill(200, 180, 128) # gold
+  stroke(40, 35, 100) # bleu
+  stroke_weight(2) # bordure épaisse
+  fill(200, 180, 128) # or
   
-  translate(0,0) # start from the top left of the screen
+  translate(0,0) # commencer en haut à gauche de l'écran
   
-  if frame_count <= 16: # creates 16 rows then stops
-    for row in range (frame_count): # animates 1 row at a time
-      for shape in range (16): # create a row of motifs
+  if frame_count <= 16: # crée 16 lignes puis s'arrête
+    for row in range (frame_count): # anime 1 ligne à la fois
+      for shape in range(16): # créer une ligne de motifs
         motif()
         translate(taille_cercle / 2, 0)
-      translate(-width, circle_size / 2) # move down to start next row
+      translate(-width, taille_cercle / 2) # descendre pour commencer à la ligne suivante
   
 run()
