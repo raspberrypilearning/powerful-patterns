4f528645e52c7eee821284658df3871d2dd8c23d
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-05 20:26:48 +0000

    New translations main.py (Ukrainian)

--- uk-UA/solutions/Art deco wallpaper/main.py
+++ uk-UA/solutions/Art deco wallpaper/main.py
@@ -7,12 +7,12 @@ from random import randint
 def motif():
   global circle_size
   for i in range(5):
-    ellipse(0, 0, circle_size / 5 * (5 - i), circle_size / 5 * (5  - i)) 
+    ellipse(0, 0, circle_size / 5 * (5 - i), circle_size / 5 * (5 - i)) 
 
 def setup():
   size(400, 400)
   frame_rate(3) 
-  print('🖌 This art uses lots of circles!')
+  print('🖌 Цей візерунок складається з багатьох кіл!')
   
   global circle_size
   
@@ -21,17 +21,17 @@ def setup():
 def draw():
   
   # Кольори візерунка
-  stroke(40, 35, 100) # blue
-  stroke_weight(2) # thick border
-  fill(200, 180, 128) # gold
+  stroke(40, 35, 100) # блакитний
+  stroke_weight(2) # товстий контур
+  fill(200, 180, 128) # золотий
   
-  translate(0,0) # start from the top left of the screen
+  translate(0,0) # почати з верхнього лівого кута екрана
   
-  if frame_count <= 16: # creates 16 rows then stops
-    for row in range (frame_count): # animates 1 row at a time
-      for shape in range (16): # create a row of motifs
+  if frame_count <= 16: # створює 16 рядків, а потім зупиняється
+    for row in range(frame_count): # анімує 1 рядок за раз
+      for shape in range(16): # створює рядок мотивів
         motif()
         translate(circle_size / 2, 0)
-      translate(-width, circle_size / 2) # move down to start next row
+      translate(-width, circle_size / 2) # переходить вниз, щоб почати новий рядок
   
 run()
