1489181adeaa5ecd146522a5b4b004202c2a373d
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-05 20:27:45 +0000

    New translations main.py (Greek)

--- el-GR/solutions/Kek lapis Sarawak/main.py
+++ el-GR/solutions/Kek lapis Sarawak/main.py
@@ -30,7 +30,7 @@ def quadrant():
   
 def outer():
 
-  # Thehe cake is wrapped in an outer layer
+  # Το κέικ περιβάλλεται από μία εξωτερική στρώση
   yellowgreen = Color(154, 205, 50) 
   
   no_fill() # Don't cover up the cake quadrants!
