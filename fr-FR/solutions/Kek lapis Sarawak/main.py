c26d9155df94e7ac46ac0f2d442418a8ef8055d0
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-05 20:27:43 +0000

    New translations main.py (French)

--- fr-FR/solutions/Kek lapis Sarawak/main.py
+++ fr-FR/solutions/Kek lapis Sarawak/main.py
@@ -9,17 +9,17 @@ def quadrant():
 
   # Choisis des couleurs magnifiques pour les couches de gâteau
   turquoise = color(64, 224, 208)
-  gold = color(255, 215, 0)
-  tomato = color(255, 99, 71)
+  or = color(255, 215, 0)
+  tomate = color(255, 99, 71)
   
   # La confiture colle les couches ensemble
-  jam = color(255, 165, 0) 
+  confiture = color(255, 165, 0) 
   stroke(confiture)
-  stroke_weight(2) # Change the number to change the amount of jam
+  stroke_weight(2) # Modifie le nombre pour changer la quantité de confiture
 
   # Neuf couches de gâteau, en répétant les 3 couleurs 3 fois
   for i in range(3):
-    start_y = i * 60 # height of 3 blocks of cake
+    debut_y = i * 60 # hauteur de 3 blocs de gâteau
     fill(turquoise)
     rect(0, debut_y, 180, 20)
     fill(or_)
@@ -30,19 +30,19 @@ def quadrant():
   
 def externe():
 
-  # Thehe cake is wrapped in an outer layer
+  # Le gâteau est enveloppé dans une couche extérieure
   jaunevert = Color(154, 205, 50) 
   
-  no_fill() # Don't cover up the cake quadrants!
+  no_fill() # Ne couvre pas les quadrants du gâteau !
   stroke(jaunevert)
   stroke_weight(20)
   rect(10, 10, 380, 380, 20) 
 
 
 def setup():
-  size(400, 400) # make the cake square
-  background(255, 255, 255, 0) # transparent background
-  frame_rate(5) # 5 frames per second
+  size(400, 400) # rendre le gâteau carré
+  background(255, 255, 255, 0) # arrière-plan transparent
+  frame_rate(5) # 5 images par seconde
 
 
 def draw():
@@ -50,18 +50,18 @@ def draw():
   # Définis un quart de tour pour que notre code soit facile à lire
   quart = radians(90)
 
-  translate(200, 200) # start from the center
+  translate(200, 200) # partir du centre
   
   # Faire le quart inférieur droit du gâteau puis faire pivoter pour les autres quarts
 
-  if frame_count <= 4: # draw up to 4 quadrants
+  if frame_count <= 4: # dessine jusqu'à 4 quadrants
     for i in range(frame_count): 
       quadrant()
       rotate(quart)
 
-  if frame_count == 5: # add the outer layer
-    translate(-200, -200) # back to the top corner
-    outer() # outer layer
+  if frame_count == 5: # ajoute la couche externe
+    translate(-200, -200) # retour au coin supérieur
+    externe() # couche externe
     
 
 run()
