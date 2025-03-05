05c5b3b460ae7f3fd21f90e46c158b5db2aa4a96
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-05 20:27:16 +0000

    New translations main.py (French)

--- fr-FR/solutions/Fibonacci-sequence/main.py
+++ fr-FR/solutions/Fibonacci-sequence/main.py
@@ -12,8 +12,8 @@ def setup():
   
 def draw():
 
-  BLUE = color(0, 0, 255)
-  BLACK = color(0, 0, 0)
+  BLEU = color(0, 0, 255)
+  NOIR = color(0, 0, 0)
   translate(width/2, height/2)
   
   # Motif
@@ -22,22 +22,22 @@ def draw():
   translate(1,1)
   rotate(radians(270))
   
-  first = 0
-  second = 1
-  next = first + second
+  premier = 0
+  deuxieme = 1
+  suivant = premier + deuxieme
   
-  # Pattern
+  # Motif
   if frame_count < 20:
     for i in range (frame_count):
-      stroke(BLACK)
-      rect(0, 0, next, next)
+      stroke(NOIR)
+      rect(0, 0, suivant, suivant)
       no_fill()
-      stroke(BLUE)
-      arc(0, -next, next*2, next*2, radians(90), radians(180))
-      translate(next,next)
+      stroke(BLEU)
+      arc(0, -suivant, suivant*2, suivant*2, radians(90), radians(180))
+      translate(suivant,suivant)
       rotate(radians(270))
-      first = second
-      second = next
-      next = first + second
+      premier = deuxieme
+      deuxieme = suivant
+      suivant = premier + deuxieme
 
 run()
