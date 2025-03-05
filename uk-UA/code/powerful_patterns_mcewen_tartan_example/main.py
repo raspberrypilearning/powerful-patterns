021a7bb7a43996516a6b58d0a901b612a1dc9acb
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-05 20:25:21 +0000

    New translations main.py (Ukrainian)

--- uk-UA/code/powerful_patterns_mcewen_tartan_example/main.py
+++ uk-UA/code/powerful_patterns_mcewen_tartan_example/main.py
@@ -8,23 +8,23 @@ def setup():
 
 
 def draw():
-    lines = 10 * frame_count  # Use in shape width/length to animate over time
+    lines = 10 * frame_count # Використовуй під час визначення ширини й довжини фігури для поступової анімації
 
-    # Кольори Тартана Макьюєна
-    # Базові кольори квадратів
-    BLUE = Color(83, 143, 200)
-    GREEN = Color(78, 163, 162)
+    # Кольори тартану клану Макʼюєнів
+    # Кольори квадратів основи
+    BLUE = Color(83, 143, 200) # блакитний
+    GREEN = Color(78, 163, 162) # зелений
     BASE_COLORS = [GREEN, BLUE]
 
     # Кольори перетинів
-    YELLOW = Color(155, 176, 135)
-    RED = Color(155, 129, 113)
+    YELLOW = Color(155, 176, 135) # жовтий
+    RED = Color(155, 129, 113) # червоний
     CROSS_COLORS = [YELLOW, RED]
 
     # Колір швів та накладання
-    GREY = Color(78, 99, 86)
+    GREY = Color(78, 99, 86) # сірий
 
-    # Намалюй основу за допомогою GREEN та BLUE квадратів, які чергуються між собою
+    # Намалюй квадрати основи за допомогою GREEN та BLUE
     no_stroke()
     y_coordinate = 0
     squares = width/square_size
@@ -32,7 +32,7 @@ def draw():
     for i in range(int(squares)):
         gap = 0
         for j in range(int(squares)):
-            fill(BASE_COLORS[j % 2])  # GREEN and BLUE
+            fill(BASE_COLORS[j % 2])  # GREEN і BLUE (зелений і блакитний)
             rect(gap, y_coordinate, square_size, square_size)
             gap = gap + square_size
         y_coordinate = y_coordinate + square_size
@@ -40,16 +40,16 @@ def draw():
     # Перетини
     stroke(GREY)
 
-    # DRAW THE YELLOW and RED alternating crosses
+    # Малює перехрестя жовтого і червоного кольорів (YELLOW і RED)
     for i in range(4):
         fill(YELLOW)
         cross = square_size / 2 - 2
         for i in range(int(squares/2)):
-            fill(CROSS_COLORS[i % 2])  # YELLOW and RED
+            fill(CROSS_COLORS[i % 2])  # YELLOW і RED (жовтий і червоний)
             rect(cross, 0, 4, lines)
             rect(0, cross, lines, 4)
             cross = cross + 2 * square_size
-        # Draw the stiching crosses
+        # Намалюй перетини для швів
         no_fill()
         cross = square_size + square_size / 2 - 2
         for i in range(int(squares)):
@@ -57,7 +57,7 @@ def draw():
             rect(0, cross, lines, 4)
             cross = cross + square_size
 
-    # Намалюй сірі лінії в місцях перекриття матеріалів
+    # Намалюй сірі лінії, де матеріал нашаровується
     no_stroke()
     fill(GREY, 100)
     gap = square_size - 4
@@ -70,8 +70,8 @@ def draw():
         gap = gap + square_size
 
 
-print('🏴󠁧󠁢󠁳󠁣󠁴󠁿󠁢󠁳󠁣󠁴󠁿 This is McEwen Tartan 🏴󠁧󠁢󠁳󠁣󠁴󠁿󠁧󠁢󠁳󠁣󠁴󠁿')
+print('🏴󠁧󠁢󠁳󠁣󠁴󠁿󠁢󠁳󠁣󠁴󠁿 Це тартан шотландського клану Макʼюенів 🏴󠁧󠁢󠁳󠁣󠁴󠁿󠁧󠁢󠁳󠁣󠁴󠁿')
 square_size = int(
-    input('What size 🏴󠁧󠁢󠁳󠁣󠁴󠁿tartan would you like? 20, 50 або 100'))
+    input('Який має бути розмір 🏴󠁧󠁢󠁳󠁣󠁴󠁿тартану? 20, 50 або 100'))
 
 run(frame_rate=10)
